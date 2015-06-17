f75e33b8c78a45dc5a7cbe0ca840c9725c5e0a55
nicamis <nicamis@gmail.com>
2015-06-16 21:48:05 -0500

    Agregando About

--- Pokemon/app/src/main/java/com/example/pc/pokemon/AboutActivity.java
+++ Pokemon/app/src/main/java/com/example/pc/pokemon/AboutActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 
 
 public class AboutActivity extends ActionBarActivity {
@@ -12,6 +13,8 @@ public class AboutActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
     @Override
